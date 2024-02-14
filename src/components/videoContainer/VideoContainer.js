04226af0ba5d8cb96389820b298cb9048704331c
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 18:15:27 +0530

    Jg: fetch the data

--- src/components/videoContainer/VideoContainer.js
+++ src/components/videoContainer/VideoContainer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useFetchYoutubeVideos } from '../../utils/hooks/useFetchYoutubeVideos';
+import VideoCard from '../videoCard/VideoCard';
 
 const VideoContainer = () => {
+
+    const { youData } = useFetchYoutubeVideos();
+    if (youData?.length === 0) return;
     return (
-        <div>VideoContainer</div>
+        <div className='flex flex-wrap gap-5'>
+            {
+                youData?.map((item) => {
+                    return <VideoCard key={item.id} data={item} />
+                })
+            }
+        </div>
     )
 }
 
