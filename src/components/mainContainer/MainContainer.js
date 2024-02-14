6661cfc845123d87b1ad2f13028595c2a3980908
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 19:14:58 +0530

    JG: add error section

--- src/components/mainContainer/MainContainer.js
+++ src/components/mainContainer/MainContainer.js
@@ -2,15 +2,22 @@ import React from 'react'
 import VideoContainer from '../videoContainer/VideoContainer';
 import ButtonList from '../btnList/ButtonList';
 import { useSelector } from 'react-redux';
+import { useFetchYoutubeVideos } from '../../utils/hooks/useFetchYoutubeVideos';
 
 const MainContainer = () => {
 
     const { isMenuOpen } = useSelector((store) => store.appConfig);
+    const { error } = useFetchYoutubeVideos();
 
     return (
         <div className={`${isMenuOpen ? 'ml-48' : 'mx-4'} flex-1 py-10`}>
-            <ButtonList />
-            <VideoContainer />
+            {
+                error ? <h1 className='text-xl text'>Error fetching YouTube videos :{error}</h1> :
+                    <>
+                        <ButtonList />
+                        <VideoContainer />
+                    </>
+            }
         </div>
     )
 }
