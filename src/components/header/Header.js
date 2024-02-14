a5969e6117fd51d6f801f66f4b103a909dc38291
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 12:28:33 +0530

    JG: create header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,16 +5,19 @@ import { FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
     return (
-        <header className='w-full py-6 px-8 bg-slate-400 flex justify-between'>
+        <header className='w-full py-6 px-8 shadow-md shadow-slate-200 flex justify-between bg-white z-50 fixed top-0'>
 
             <div className='flex items-center gap-3'>
                 <p className='text-lg'><RiMenu2Line /></p>
                 <h2 className='text-2xl'>The Watcher</h2>
             </div>
 
-            <div className='border-[1px] border-gray-500 rounded-full bg-gray-100 overflow-hidden flex items-center  w-1/2'>
-                <input type='text' placeholder='Search' className='bg-transparent w-11/12 h-full px-3 outline-none border-none' />
-                <button className='text-xl border-l-2 border-gray-500 h-full  p-3'><RiSearch2Line /></button>
+            <div className='flex w-1/2'>
+                <input type='text' placeholder='Search'
+                    className='search-input' />
+                <button className='search-btn'>
+                    <RiSearch2Line />
+                </button>
             </div>
 
             <div className='flex justify-center text-3xl items-center'>
