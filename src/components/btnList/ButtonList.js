f2e97498adff505dd21962a1f3ea9c38824b06bd
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 16:33:31 +0530

    JG: create button list

--- src/components/btnList/ButtonList.js
+++ src/components/btnList/ButtonList.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import btnData from '../../utils/data/btnList.json';
 
 const ButtonList = () => {
     return (
-        <div>ButtonList</div>
+        <div className='flex w-full gap-3 mb-4 '>
+            {
+                btnData?.map((btn, index) => {
+                    return <WatcherButton name={btn.name} key={index} />
+                })
+            }
+        </div>
     )
 }
 
 export default ButtonList
+
+export const WatcherButton = ({ name }) => {
+    return <button className='watcher-btn '>
+        {name}
+    </button>
+}
