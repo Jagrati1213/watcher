ed6db6d3e4a160feb00245b04acd79409b8d25ee
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 18:15:09 +0530

    JG: add url

---
+++ src/utils/constant.js
@@ -0,0 +1,1 @@
+export const VIDEO_API_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.REACT_APP_API_KEY}`;
