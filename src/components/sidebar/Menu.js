37226ed4d14902ed3d5518ef4395055e18701aa3
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 16:33:16 +0530

    JG: add hide feature

--- src/components/sidebar/Menu.js
+++ src/components/sidebar/Menu.js
@@ -6,25 +6,29 @@ import { MdPodcasts } from "react-icons/md";
 import { FaFire } from "react-icons/fa";
 import { LuMusic4 } from "react-icons/lu";
 import { BsLightbulb } from "react-icons/bs";
+import { useSelector } from 'react-redux';
 
 const Menu = () => {
-    return (
-        <div className='w-60 border-r-2 h-full fixed left-0'>
-            <ul className='p-5'>
-                <li className='flex items-center gap-5 text-base py-1'><GrHomeRounded /> Home</li>
-                <li className='flex items-center gap-5 text-base py-1'><SiYoutubeshorts /> Shorts</li>
-                <li className='flex items-center gap-5 text-base py-1'><MdOutlineSubscriptions /> Subscription</li>
-            </ul>
-            <hr />
-            <ul className='px-5 py-3'>
-                <li className='py-2 font-semibold text-lg'>Subscriptions</li>
-                <li className='flex items-center gap-5 text-base py-1'><FaFire /> Trending</li>
-                <li className='flex items-center gap-5 text-base py-1'><LuMusic4 /> Music</li>
-                <li className='flex items-center gap-5 text-base py-1'><BsLightbulb /> Learning</li>
-                <li className='flex items-center gap-5 text-base py-1'><MdPodcasts /> Podcasts</li>
+    const { isMenuOpen } = useSelector((store) => store.appConfig);
 
-            </ul>
-        </div>
+    return (
+        isMenuOpen ?
+            <div className='w-auto border-r-2 h-full fixed left-0 z-40' >
+                <ul className='p-5'>
+                    <li className='menu-item'><GrHomeRounded /> Home</li>
+                    <li className='menu-item'><SiYoutubeshorts /> Shorts</li>
+                    <li className='menu-item'><MdOutlineSubscriptions /> Subscription</li>
+                </ul>
+                <hr />
+                <ul className='p-5'>
+                    <li className='py-2 font-semibold text-lg'>Subscriptions</li>
+                    <li className='menu-item'><FaFire /> Trending</li>
+                    <li className='menu-item'><LuMusic4 /> Music</li>
+                    <li className='menu-item'><BsLightbulb /> Learning</li>
+                    <li className='menu-item'><MdPodcasts /> Podcasts</li>
+
+                </ul>
+            </div> : null
     )
 }
 
