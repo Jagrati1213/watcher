b799ed48413102a2a7b570e3e9f09a211dadaf33
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 16:35:19 +0530

    JG: add toggle

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { RiMenu2Line } from "react-icons/ri";
 import { RiSearch2Line } from "react-icons/ri";
 import { FaUserCircle } from "react-icons/fa";
+import { useDispatch } from 'react-redux';
+import { toggleMenu } from '../../utils/redux/slices/appSlice';
 
 const Header = () => {
+
+    const dispatch = useDispatch();
+    const handleToggleMenu = () => {
+        dispatch(toggleMenu());
+    }
     return (
         <header className='w-full py-6 px-8 shadow-md shadow-slate-200 flex justify-between bg-white z-50 fixed top-0'>
 
             <div className='flex items-center gap-3'>
-                <p className='text-lg'><RiMenu2Line /></p>
-                <h2 className='text-2xl'>The Watcher</h2>
+                <p className='text-xl cursor-pointer hover:text-yellow-700' onClick={handleToggleMenu}><RiMenu2Line /></p>
+                <h2 className='text-2xl text-yellow-700 font-bold'>The Watcher</h2>
             </div>
 
             <div className='flex w-1/2'>
