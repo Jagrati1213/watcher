ee8673fe94989ec287c299dcfb0799a7001c464d
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 19:14:34 +0530

    JG: add header

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Menu from '../sidebar/Menu';
 import { Outlet } from 'react-router';
+import Header from '../header/Header';
 
 function Body() {
     return (
         <div className='flex gap-2 h-screen mt-24'>
+            <Header />
             <Menu />
             <Outlet />
         </div>
