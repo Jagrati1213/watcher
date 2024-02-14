44a6b715ad5d32f53ba152fe7bcf66e9d07e70dd
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 16:33:55 +0530

    JG: add condition works on hidden

--- src/components/mainContainer/MainContainer.js
+++ src/components/mainContainer/MainContainer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import VideoContainer from '../videoContainer/VideoContainer';
 import ButtonList from '../btnList/ButtonList';
+import { useSelector } from 'react-redux';
 
 const MainContainer = () => {
+
+    const { isMenuOpen } = useSelector((store) => store.appConfig);
+
     return (
-        <div>MainContainer
+        <div className={`${isMenuOpen ? 'ml-48' : 'mx-4'} flex-1 py-10`}>
             <ButtonList />
             <VideoContainer />
         </div>
