2fbdef3d052e5eb19daa3b284c5350b834d991ae
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-14 15:29:56 +0530

    JG: add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Body from "./components/body/Body";
 import Header from "./components/header/Header";
+import MainContainer from "./components/mainContainer/MainContainer";
+import Watch from "./components/watch/Watch";
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Body />,
+    children: [
+      {
+        path: '/',
+        element: <MainContainer />
+      },
+      {
+        path: '/watch',
+        element: <Watch />
+      }
+    ]
+  }
+])
 
 function App() {
   return (
     <div className="App min-h-screen">
       <Header />
-      <Body />
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
