5c48168c1dfa0ebe90f75f4a902cc176841da00f
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-15 15:03:48 +0530

    JG: import slice

--- src/utils/redux/store.js
+++ src/utils/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./slices/appSlice";
+import searchSlice from "./slices/searchSlice";
 
 export const store = configureStore({
     reducer: {
-        appConfig: appSlice
+        appConfig: appSlice,
+        search: searchSlice,
     }
 })
