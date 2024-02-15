b6a57335552e7737f6fcf8923b8654972ca4e5cf
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-15 17:58:27 +0530

    JG: remove toggle condition && add fetch url

--- src/components/mainContainer/MainContainer.js
+++ src/components/mainContainer/MainContainer.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import VideoContainer from '../videoContainer/VideoContainer';
 import ButtonList from '../btnList/ButtonList';
-import { useSelector } from 'react-redux';
 import { useFetchYoutubeVideos } from '../../utils/hooks/useFetchYoutubeVideos';
+import { VIDEO_API_URL } from '../../utils/constant';
 
 const MainContainer = () => {
-
-    const { isMenuOpen } = useSelector((store) => store.appConfig);
-    const { error, youData } = useFetchYoutubeVideos();
+    const { error, youData } = useFetchYoutubeVideos(VIDEO_API_URL);
 
     return (
-        <div className={`${isMenuOpen ? 'ml-48' : 'mx-4'} flex-1 py-10`}>
+        <div className='flex-1 py-10'>
             {
                 error ? <h1 className='text-xl text'>Error fetching YouTube videos :{error}</h1> :
                     <>
