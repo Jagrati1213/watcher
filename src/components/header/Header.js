f38713d4d5e026e10d253f76ffb871944254bc9d
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-15 15:02:25 +0530

    JG: call Fetch search items && and display them

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiMenu2Line } from "react-icons/ri";
 import { RiSearch2Line } from "react-icons/ri";
 import { FaUserCircle } from "react-icons/fa";
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../../utils/redux/slices/appSlice';
 import { Link } from 'react-router-dom';
+import { useFetchYoutubeSearches } from '../../utils/hooks/useFetchYoutubeSearches';
 
 const Header = () => {
 
     const dispatch = useDispatch();
+    const [searchQuery, setSearchQuery] = useState('');
+    const [hideSuggestion, setHideSuggestion] = useState(false);
+
     const handleToggleMenu = () => {
         dispatch(toggleMenu());
     }
+
+    const searchData = useFetchYoutubeSearches(searchQuery);
+
     return (
-        <header className='w-full py-6 px-8 shadow-md shadow-slate-200 flex justify-between bg-white z-50 fixed top-0'>
+        <header className='header'>
 
             <div className='flex items-center gap-3'>
                 <p className='text-xl cursor-pointer hover:text-yellow-700' onClick={handleToggleMenu}><RiMenu2Line /></p>
@@ -22,12 +29,35 @@ const Header = () => {
                 </h2>
             </div>
 
-            <div className='flex w-1/2'>
-                <input type='text' placeholder='Search'
-                    className='search-input' />
-                <button className='search-btn'>
-                    <RiSearch2Line />
-                </button>
+            <div className='flex w-1/2 flex-col relative'>
+                <div className='flex w-full'>
+                    <input
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onFocus={() => setHideSuggestion(true)}
+                        onBlur={() => setHideSuggestion(false)}
+                        type='text'
+                        placeholder='Search'
+                        className='search-input' />
+                    <button className='search-btn'>
+                        <RiSearch2Line />
+                    </button>
+                </div>
+                {
+                    (hideSuggestion && searchData?.length > 0) && <div className='search-list-container'>
+                        <ul className='py-2'>
+                            {
+                                searchData?.map((item, index) => {
+                                    return <li key={index}
+                                        className='search-list'>
+                                        <RiSearch2Line />{item}
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                }
+
             </div>
 
             <div className='flex justify-center text-3xl items-center'>
