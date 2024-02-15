cc59ff776e5df9ee7dd2845af5adf9161a6dfffc
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-15 18:22:53 +0530

    JG: make optimize the UI

--- src/components/videoCard/VideoCard.js
+++ src/components/videoCard/VideoCard.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const VideoCard = ({ data }) => {
 
+    if (!data?.snippet?.thumbnails?.medium?.url) return;
+
     let views;
 
     if (1 <= data?.statistics?.viewCount && data?.statistics?.viewCount < 1000) {
@@ -14,16 +16,22 @@ const VideoCard = ({ data }) => {
         views = `${Math.floor(data?.statistics?.viewCount / 10000)}M`;
     }
 
+
+
     return (
+
         <div className='p-5 bg-gray-100 hover:shadow-lg transform cursor-pointer h-full w-full'>
             <div>
                 <img src={data?.snippet?.thumbnails?.medium?.url}
                     alt={data?.channelTitle}
-                    className='rounded w-full object-cover' />
+                    className='rounded w-full h-40 object-cover' />
             </div>
             <h3 className='font-semibold'>{`${data?.snippet?.title.slice(0, 60)}...`}</h3>
             <h5 className='text-sm text-gray-500 font-semibold py-1 tracking-wide'>{data?.snippet?.channelTitle}</h5>
-            <p className='text-sm text-gray-500 tracking-wide'>{views} views</p>
+            {
+                (data?.statistics?.viewCount !== undefined) &&
+                <p className='text-sm text-gray-500 tracking-wide'>{views} views</p>
+            }
         </div>
     )
 }
