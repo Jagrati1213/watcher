cea4233efa980ff0e07091782eafe5d8fc017d01
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-15 17:57:15 +0530

    JG: resolve id

--- src/components/videoContainer/VideoContainer.js
+++ src/components/videoContainer/VideoContainer.js
@@ -5,14 +5,16 @@ import { Link } from 'react-router-dom';
 const VideoContainer = ({ data }) => {
 
     if (data?.length === 0) return;
+
     return (
         <div className='flex flex-wrap gap-5'>
-            {
-                data?.map((item) => {
-                    return <Link to={`/watch?v=${item.id}`} key={item.id} className='w-full md:w-80'>
-                        <VideoCard data={item} />
-                    </Link>
-                })
+            {data?.map((item, index) => {
+                const id = item?.id?.videoId || item?.id;
+
+                return <Link to={`/watch?v=${id}`} key={index} className='w-full md:w-80'>
+                    <VideoCard data={item} />
+                </Link>
+            })
             }
         </div>
     )
