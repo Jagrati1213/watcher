d02d3dda7ad4aee4b3fd2ecccfd6221d28212d9b
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-15 17:59:08 +0530

    JG;: add toggle condition

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Menu from '../sidebar/Menu';
 import { Outlet } from 'react-router';
 import Header from '../header/Header';
+import { useSelector } from 'react-redux';
 
 function Body() {
+    const { isMenuOpen } = useSelector((store) => store.appConfig);
+
     return (
         <div className='flex gap-2 h-screen mt-24'>
             <Header />
             <Menu />
-            <Outlet />
+            <div className={`${isMenuOpen ? 'ml-48' : 'ml-5'} flex-1`}>
+                <Outlet />
+            </div>
         </div>
     )
 }
