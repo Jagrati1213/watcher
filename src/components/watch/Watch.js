01cd2b9831d223274800d1b06116b70a91f7c6eb
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-16 14:18:26 +0530

    JG: import live chat

--- src/components/watch/Watch.js
+++ src/components/watch/Watch.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom';
 import CommentSection from '../commentsection/CommentSection';
+import LiveChat from '../liveChat/LiveChat';
 
 const Watch = () => {
 
     const [searchParams] = useSearchParams();
 
     return (
-        <div className='flex-1'>
+        <div className='flex-1 flex gap-1 flex-wrap'>
             <div>
                 <iframe width="1160"
                     height="615"
@@ -16,8 +17,9 @@ const Watch = () => {
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowFullScreen></iframe>
-                <CommentSection />
+                {/* <CommentSection /> */}
             </div>
+            <LiveChat />
         </div >
     )
 }
