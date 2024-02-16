45bb653beafbaca608ffb7226b7db1cfc4133e7f
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-16 15:06:57 +0530

    JG: Make responsive

--- src/components/watch/Watch.js
+++ src/components/watch/Watch.js
@@ -8,10 +8,10 @@ const Watch = () => {
     const [searchParams] = useSearchParams();
 
     return (
-        <div className='flex-1 flex gap-1 flex-wrap'>
-            <div>
-                <iframe width="1160"
-                    height="615"
+        <div className='flex gap-1 flex-col lg:flex-row'>
+            <div className='w-full xl:w-1/2 h-[700px] flex-none lg:flex-1 bg-gray-200'>
+                <iframe
+                    className='w-full h-full'
                     src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
                     title="YouTube video player"
                     frameBorder="0"
