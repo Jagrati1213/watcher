21ffe9af6129591fb778f5020de81512b9fc9fbc
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-16 15:40:49 +0530

    JG: responsive header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -14,9 +14,9 @@ const Header = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const [hideSuggestion, setHideSuggestion] = useState(false);
 
-    const handleToggleMenu = () => {
-        dispatch(toggleMenu());
-    }
+    // const handleToggleMenu = () => {
+    //     dispatch(toggleMenu());
+    // }
 
     const searchData = useFetchYoutubeSearches(searchQuery);
 
@@ -36,13 +36,13 @@ const Header = () => {
         <header className='header'>
 
             <div className='flex items-center gap-3'>
-                <p className='text-xl cursor-pointer hover:text-yellow-700' onClick={handleToggleMenu}><RiMenu2Line /></p>
-                <h2 className='text-2xl text-yellow-700 font-bold'>
+                {/* <p className='text-xl cursor-pointer hover:text-yellow-700' onClick={handleToggleMenu}><RiMenu2Line /></p> */}
+                <h2 className='text-xl md:text-2xl text-yellow-700 font-bold'>
                     <Link to={'/'}>The Watcher</Link>
                 </h2>
             </div>
 
-            <div className='flex w-1/2 flex-col relative'>
+            <div className='flex w-full md:w-1/2 flex-col relative'>
                 <div className='flex w-full'>
                     <input
                         ref={searchInputRef}
@@ -75,11 +75,6 @@ const Header = () => {
                 }
 
             </div>
-
-            <div className='flex justify-center text-3xl items-center'>
-                <FaUserCircle />
-            </div>
-
         </header>
     )
 }
