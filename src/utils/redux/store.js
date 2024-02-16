a6a60bebfb720c04a94ef51404071d08e16b0ca1
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-16 14:19:39 +0530

    JG: import chat slice

--- src/utils/redux/store.js
+++ src/utils/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./slices/appSlice";
 import searchSlice from "./slices/searchSlice";
+import chatSlice from "./slices/chatSlice";
 
 export const store = configureStore({
     reducer: {
         appConfig: appSlice,
         search: searchSlice,
+        chats: chatSlice,
     }
 })
