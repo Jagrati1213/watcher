d1375c2c55164c68601a60e65621bed3b27e11fc
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-16 16:28:22 +0530

    JG: import loader

--- src/components/mainContainer/MainContainer.js
+++ src/components/mainContainer/MainContainer.js
@@ -3,18 +3,20 @@ import VideoContainer from '../videoContainer/VideoContainer';
 import ButtonList from '../btnList/ButtonList';
 import { useFetchYoutubeVideos } from '../../utils/hooks/useFetchYoutubeVideos';
 import { VIDEO_API_URL } from '../../utils/constant';
+import Loader from '../loader/Loader';
 
 const MainContainer = () => {
-    const { error, youData } = useFetchYoutubeVideos(VIDEO_API_URL);
+    const { error, youData, loader } = useFetchYoutubeVideos(VIDEO_API_URL);
 
     return (
         <div className='flex-1 py-10'>
             {
-                error ? <h1 className='text-xl text'>Error fetching YouTube videos :{error}</h1> :
-                    <>
-                        {/* <ButtonList /> */}
-                        <VideoContainer data={youData} />
-                    </>
+                error ? <h1 className='text-xl text'>Error fetching YouTube videos :{error}</h1>
+                    : loader ? <Loader />
+                        : <>
+                            {/* <ButtonList /> */}
+                            <VideoContainer data={youData} />
+                        </>
             }
         </div>
     )
