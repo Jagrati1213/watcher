cb1bf8585c0d9975bee70e08cc29b3675b7e42b8
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-16 16:28:34 +0530

    JG: add loader functionality

--- src/utils/hooks/useFetchYoutubeVideos.js
+++ src/utils/hooks/useFetchYoutubeVideos.js
@@ -11,18 +11,22 @@ export const useFetchYoutubeVideos = (fetchUrl, dependency = []) => {
 
     const [youData, setYouData] = useState([]);
     const [error, setError] = useState(null);
+    const [loader, setLoader] = useState(false);
 
     const getVideos = async () => {
+        setLoader(true);
         try {
             const response = await fetch(fetchUrl);
             const data = await response.json();
             if (!response.ok) throw new Error(`${response.status}`);
 
             setYouData(data?.items);
+            setLoader(false);
             setError(null);
             return data?.items;
 
         } catch (err) {
+            setLoader(false);
             console.error("Error fetching YouTube videos:", err);
             setError(err.message || "An error occurred");
         }
@@ -33,5 +37,5 @@ export const useFetchYoutubeVideos = (fetchUrl, dependency = []) => {
         getVideos();
     }, dependency);
 
-    return { youData, error };
+    return { youData, error, loader };
 }
